d205583be648a3ac77b426c757c6d0a336728f7a
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2021-08-23 09:59:32 +0200

    TST fixed precision

--- multixrank/tests/test_hetionet.py
+++ multixrank/tests/test_hetionet.py
@@ -1,4 +1,8 @@
 import filecmp
+
+import numpy
+import pandas
+
 import multixrank
 import os
 import pathlib
@@ -36,9 +40,16 @@ class TestHetionet(unittest.TestCase):
         multiplex_2_path = os.path.join(outdir_path, "multiplex_2.tsv")
         multiplex_2_path_bak = os.path.join(outdir_path_bak, "multiplex_2.tsv")
         self.assertTrue(filecmp.cmp(multiplex_2_path, multiplex_2_path_bak))
+
+
         multiplex_3_path = os.path.join(outdir_path, "multiplex_3.tsv")
         multiplex_3_path_bak = os.path.join(outdir_path_bak, "multiplex_3.tsv")
-        self.assertTrue(filecmp.cmp(multiplex_3_path, multiplex_3_path_bak))
+        # self.assertTrue(filecmp.cmp(multiplex_3_path, multiplex_3_path_bak))
+        multiplex_3_df = pandas.read_csv(multiplex_3_path, sep="\t")
+        multiplex_3_df_bak = pandas.read_csv(multiplex_3_path_bak, sep="\t")
+        multiplex_3_score_lst = multiplex_3_df['score'].tolist()
+        multiplex_3_score_lst_bak = multiplex_3_df_bak['score'].tolist()
+        numpy.testing.assert_almost_equal(multiplex_3_score_lst, multiplex_3_score_lst_bak, decimal=7)
 
     def tearDown(self):
         shutil.rmtree(self.outdir, ignore_errors=True)
