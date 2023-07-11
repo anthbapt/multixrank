4d238740412878c78459dded5da6a1a2d788ad4e
Anthony Baptista <35119963+anthbapt@users.noreply.github.com>
2023-07-11 07:27:00 +0100

    Add files via upload
    Update for the last version of script and network

--- multixrank/Output.py
+++ multixrank/Output.py
@@ -1,7 +1,7 @@
 import os
 import pandas
 import pathlib
-from scipy.stats import stats
+from scipy.stats import mstats
 
 
 class Output:
@@ -15,7 +15,7 @@ class Output:
 
         multiplex_node_prob_zero_df = self._df.loc[self._df.score == 0][['multiplex', 'node']].drop_duplicates()
         multiplex_node_prob_zero_df['score'] = 0
-        self._df = (self._df.loc[self._df.score > 0]).groupby(['multiplex', 'node']).agg({'score': stats.gmean}).reset_index()
+        self._df = (self._df.loc[self._df.score > 0]).groupby(['multiplex', 'node']).agg({'score': mstats.gmean}).reset_index()
         self._df = pandas.concat([multiplex_node_prob_zero_df, self._df], axis=0)
         self._df = self._df.drop_duplicates(['multiplex', 'node'], keep='first')
         self._df.sort_values('score', ascending=False, inplace=True)
