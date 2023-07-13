66106ce3815f7a0fb951907ad011f977d74f8aad
Anthony Baptista <35119963+anthbapt@users.noreply.github.com>
2023-07-13 07:38:47 +0100

    Delete build/lib/multixrank directory

--- build/lib/multixrank/logger_setup.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger("multixrank")
-handler = logging.StreamHandler()
-formatter_str = '%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s'
-formatter_obj = logging.Formatter(formatter_str)
-handler.setFormatter(formatter_obj)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)  # set root's level
